cc9212bd8a9b015f96e8caa5c6de395b1633c7b7
Vladimir Namestnikov <vova_vlad_putin_2@mail.ru>
2024-11-18 00:45:47 +0500

    fix test log constant

--- internal/domain/parser/parser_test.go
+++ internal/domain/parser/parser_test.go
@@ -11,9 +11,10 @@ import (
 
 const (
 	layout   = "02/Jan/2006:15:04:05 -0700"
-	nginxLog = `244.103.237.229 - - [17/Nov/2024:16:07:52 +0000] "GET /reciprocal.hmtl HTTP/1.1"` +
-		`200 2420 "-" "Mozilla/5.0 (X11; Linux x86_64)"`
-	nonNginxLog = `145.127.201.137 - [17/Nov/2024:12:59:30 +0000] "POST /encompassing.gif HTTP/1.1" 200 806 "-" "Opera/9.52"`
+	nginxLog = `244.103.237.229 - - [17/Nov/2024:16:07:52 +0000] "GET /reciprocal.hmtl HTTP/1.1" 200 2420 "-" ` +
+		`"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/5331 (KHTML, like Gecko) Chrome/36.0.897.0 Mobile Safari/5331"`
+	nonNginxLog = `145.127.201.137 - [17/Nov/2024:12:59:30 +0000] "POST ` +
+		`/Synergistic/encompassing.gif HTTP/1.1" 200 806 "-" "Opera/9.52 (X11; Linux i686; en-US) Presto/2.12.195 Version/13.00"`
 )
 
 func TestParse(t *testing.T) {
@@ -43,7 +44,7 @@ func TestParse(t *testing.T) {
 				Status:        200,
 				BodyBytesSent: 2420,
 				HTTPRefer:     "-",
-				HTTPUserAgent: "Mozilla/5.0 (X11; Linux x86_64)",
+				HTTPUserAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/5331 (KHTML, like Gecko) Chrome/36.0.897.0 Mobile Safari/5331",
 			},
 			wantErr: false,
 		},
@@ -57,7 +58,9 @@ func TestParse(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := (&parser.Parser{}).Parse(tt.lg)
+			p := &parser.Parser{}
+
+			got, err := p.Parse(tt.lg)
 
 			assert.Equal(t, tt.want, got)
 			assert.Equal(t, tt.wantErr, err != nil)
